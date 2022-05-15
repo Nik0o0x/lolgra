bb7323688d18e05741d43ed2b63157bfb16c97a4
nikod <nikod@DESKTOP-VL94D05>
2022-05-16 01:08:04 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using LolClasses;
+using static LolClasses.RubyCrystal;
+
+//stwórz nowego champa - garena
+Garen g = new Garen();
+//opdal emotke dla garena
+g.ctrl3();
+
+//stworz instacje przedmiotu - rubycrystal
+RubyCrystal rb = new RubyCrystal();
+GiantsBelt cd = new GiantsBelt();
+//sprawdz ile ma hp garen
+g.showHP();
+//kup mu item
+g.buyItem(cd);
+//sprawdz ile ma teraz hp
+g.showHP();
+//kup mu drugi item
+g.buyItem(rb);
+//sprawdz ile ma teraz hp
+g.showHP();
+
+
+
+
+
+
---
+++ ConsoleApp3/RubyCrystal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LolClasses
+{
+    /*
+     * klasa opisująca konkretny rodzaj itemu w sklepie
+     */
+    public class RubyCrystal : Item
+    {
+        //deprecated???
+        //public int bonusHP = 150;
+        public RubyCrystal()
+        {
+            //ustalamy nazwe itemu
+            name = "Ruby Crystal";
+            // jego cene
+            price = 400;
+            //i atrybuty
+            AddStat(Stat.HP, 150);
+        }
+        public class GiantsBelt : Item
+        {
+            //deprecated???
+            //public int bonusHP = 350;
+            public GiantsBelt()
+            {
+                //ustalamy nazwe itemu
+                name = "Giant's Belt";
+                // jego cene
+                price = 900;
+                //i atrybuty
+                AddStat(Stat.HP, 350);
+            }
+        }
+    }
+}
